df93d2b8847b5d06750534a964ad3f48504a5d50
raptor <raptor@0xdeadbeef.info>
2025-12-01 09:56:18 +0100

    chore: improve code formatting

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,12 +4,10 @@ use axum::Router;
 
 async fn main() {
     let server_addr = "localhost:3100";
-
     let router = Router::new();
 
     println!("Launching back end: http://{server_addr}");
 
     let listener = tokio::net::TcpListener::bind(&server_addr).await.unwrap();
-
     axum::serve(listener, router).await.unwrap();
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -4,12 +4,10 @@ use axum::Router;
 
 async fn main() {
     let server_addr = "localhost:3000";
-
     let router = Router::new();
 
     println!("Launching front end: http://{server_addr}");
 
     let listener = tokio::net::TcpListener::bind(&server_addr).await.unwrap();
-
     axum::serve(listener, router).await.unwrap();
 }
